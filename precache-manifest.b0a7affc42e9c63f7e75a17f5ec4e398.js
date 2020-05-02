9c1a14fd541d059b48ed4a61bd6ae8171dd9036b
PatrickYang <azureroki@gmail.com>
2020-05-02 17:16:31 +0800

    Updates

---
+++ precache-manifest.b0a7affc42e9c63f7e75a17f5ec4e398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c41851908a6ff46b9c9c1287998fc8",
+    "url": "/React-Exercise/index.html"
+  },
+  {
+    "revision": "6aab6723443d04f42498",
+    "url": "/React-Exercise/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8412d11fa9ee5d914f98",
+    "url": "/React-Exercise/static/js/2.8e1a3d53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Exercise/static/js/2.8e1a3d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aab6723443d04f42498",
+    "url": "/React-Exercise/static/js/main.063060bd.chunk.js"
+  },
+  {
+    "revision": "d9c7fb36eb95bdd408be",
+    "url": "/React-Exercise/static/js/runtime-main.68f18029.js"
+  }
+]);
